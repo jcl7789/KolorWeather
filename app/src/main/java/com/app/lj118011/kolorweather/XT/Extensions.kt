9f529a93984298ec32985ab2f049859af2006923
Juan Carlos Leto <juan.leto@icbc.com.ar>
2018-10-08 16:34:23 -0300

    Agrego Extensiones para objetos

--- app/src/main/java/com/app/lj118011/kolorweather/XT/Extensions.kt
+++ app/src/main/java/com/app/lj118011/kolorweather/XT/Extensions.kt
@@ -1,2 +1,18 @@
 package com.app.lj118011.kolorweather.XT
 
+import android.support.design.widget.Snackbar
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.json.JSONArray
+import org.json.JSONObject
+
+operator fun JSONArray.iterator(): Iterator<JSONObject>
+        = (0 until length()).asSequence().map { get(it) as JSONObject }.iterator()
+
+fun ViewGroup.inflate(resource:Int): View = LayoutInflater.from(context).inflate(resource, this, false)
+
+fun View.displaySnack(mensaje:String, lenght_time: Int = Snackbar.LENGTH_INDEFINITE){
+    val snackbar = Snackbar.make(this, mensaje, lenght_time)
+    snackbar.show()
+}
